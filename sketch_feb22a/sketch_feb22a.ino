cb32a32310ac394d16cf7a45e38da4853ee23527
LuneEve <75272109+LuneEve@users.noreply.github.com>
2021-02-25 10:45:33 -0500

    Add files via upload
    22a is Mic Example
    22b is Temperature Example

---
+++ sketch_feb22a/sketch_feb22a.ino
@@ -0,0 +1,14 @@
+int soundPin = A0;
+int sensorValue = 0;
+ 
+void setup () 
+{
+  Serial.begin (9600);
+}
+ 
+void loop () 
+{
+  sensorValue = analogRead (soundPin);
+  Serial.println (sensorValue, DEC);
+  delay (1000);
+}
