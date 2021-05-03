431c058d2a3eeb7afccc1322d245f72baeffd8b6
zbrachinara <zbrachinara@gmail.com>
2021-05-03 13:53:37 -0400

    Removed submodule to better structure arduino

--- Arduino-Sound-Temperature.ino
+++ Arduino-Sound-Temperature.ino
@@ -1,8 +1,11 @@
-#include "ArduinoRTClibrary/virtuabotixRTC.h"
+#include "src/ArduinoRTClibrary/virtuabotixRTC.h"
+#define CLK 6
+#define DAT 7
+#define RST 8
 
 void setup() {
 
-
+  virtuabotixRTC rtc(CLK, DAT, RST);
   
 }
 
