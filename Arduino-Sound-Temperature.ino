1f66929855ef9fc0f85b54c86553d21f8fccaa53
zbrachinara <zbrachinara@gmail.com>
2021-05-03 14:09:31 -0400

    Test prints

--- Arduino-Sound-Temperature.ino
+++ Arduino-Sound-Temperature.ino
@@ -3,14 +3,20 @@
 #define DAT 7
 #define RST 8
 
+virtuabotixRTC mh_rtc(CLK, DAT, RST);
+
 void setup() {
 
-  virtuabotixRTC rtc(CLK, DAT, RST);
+  Serial.begin(9600);
+  mh_rtc.setDS1302Time(0, 0, 0, 0, 0, 0, 0);
   
 }
 
 void loop() {
 
+  delay(5000);
+  mh_rtc.updateTime();
 
+  Serial.println(mh_rtc.seconds);
   
 }
