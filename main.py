bd46531be029a9d9d16c186220552e9a5075ef89
zbrachinara <zbrachinara@gmail.com>
2021-05-05 20:47:00 -0400

    Updated read() to make output more usable

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import serial
 import sys
 
-port_name='undefined_os_behavior'
+port_name = 'undefined_os_behavior'
 if sys.platform == 'linux':
     port_name = '/dev/ttyACM0'
 elif sys.platform == 'win32':
@@ -9,8 +9,17 @@ elif sys.platform == 'win32':
 
 arduino = serial.Serial(port=port_name, baudrate=9600, timeout=0.1)
 
+
 def read():
-    return arduino.readline()
+    out = str(arduino.readline())
+    if sys.platform == 'linux':
+        out = out.split('\'')[1].split('\\r')[0]  # so basically:
+    # arduino.readline() will return a string of form:
+    #   b'[content]\r\n'
+    # at least, on linux.
+    # that line there exists to strip all of that off
+    return out
+
 
 while True:
     print(read())
