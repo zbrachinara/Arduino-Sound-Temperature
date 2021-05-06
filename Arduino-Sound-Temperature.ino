1053d02f08bcc133e93a23e4294486fbdcdb0f6c
zbrachinara <zbrachinara@gmail.com>
2021-05-06 13:59:28 -0400

    Send time on circuit closed

--- Arduino-Sound-Temperature.ino
+++ Arduino-Sound-Temperature.ino
@@ -3,20 +3,27 @@
 #define DAT 7
 #define RST 8
 
+#define SIGSEND 4
+
 virtuabotixRTC mh_rtc(CLK, DAT, RST);
 
 void setup() {
 
+  pinMode(SIGSEND, INPUT);
   Serial.begin(9600);
   mh_rtc.setDS1302Time(0, 0, 0, 0, 0, 0, 0);
   
 }
 
 void loop() {
-
-  delay(5000);
+  
   mh_rtc.updateTime();
 
-  Serial.println(mh_rtc.seconds);
+  if (digitalRead(SIGSEND)==HIGH) {
+    Serial.println(mh_rtc.seconds);
+    while(digitalRead(SIGSEND)==HIGH) {
+      
+    }
+  }
   
 }
